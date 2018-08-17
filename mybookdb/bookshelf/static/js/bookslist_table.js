c3e291c503d490442db00fd5c7f82f2f7b974337
freyp567 <freyp567@gmail.com>
2018-08-17 19:58:01 +0200

    fix issue with bootstrap-table extensions filter-control and cookie

--- mybookdb/bookshelf/static/js/bookslist_table.js
+++ mybookdb/bookshelf/static/js/bookslist_table.js
@@ -4,6 +4,24 @@ wiring for bootstrap-table displaying books
 http://bootstrap-table.wenzhixin.net.cn/documentation/
 */
 
+// workaround for issue with bootstrap-table filter-control and cookie 
+const filter_control = Cookies.get('books.bs.table.filterControl');
+if (!filter_control) {
+  JSONCookies = Cookies.withConverter({
+      read: function(value, name) {
+          //return cookie.replace(rdecode, decodeURIComponent);
+          return value;
+      },
+      write: function (value, name) {
+          return value;
+      }
+  });
+  // path: '/bookshelf/books/v2'
+  const cookie_value = [{'field':'title','text':''}];
+  JSONCookies.set('books.bs.table.filterControl', cookie_value, { expires: 1 });
+}
+
+
 if ($('#bookslist').length !== 0) {
   console.error("initialize bootstrap table bookslist");
   
