b12722598f1baa0143dd94f3bc0e850f0b896bc0
freyp567 <freyp567@gmail.com>
2018-10-23 08:27:54 +0200

    unix line ends

--- mybookdb/mybookdb/settings.py
+++ mybookdb/mybookdb/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", ["127.0.0.1",])
 
 
 # Application definition
