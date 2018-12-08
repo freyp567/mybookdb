62b29d3fba4e813171c3e40f951bac37666f67c7
freyp567 <freyp567@gmail.com>
2018-12-08 21:50:23 +0100

    update

--- mybookdb/bookshelf/models.py
+++ mybookdb/bookshelf/models.py
@@ -295,3 +295,45 @@ class states(models.Model):
         if self.toBuy:
             state_info.append('wish')
         return ' '.join(state_info)
+
+
+"""
+TODO use a pluggable app to provide link management?
+verify: https://github.com/kplaube/django-simple-links?
+
+class linktargets(models.Model):
+    # valid link targets
+    name = models.TextField(null=False)
+    description = models.TextField(null=True)
+    base_url = models.TextField(null=False)
+ 
+
+class links(models.Model):
+    # link (URL, ...) to external content
+    link_name = models.TextField(null=False)  # short name describing link
+    link_target = models.TextField(null=False) # values from linktargets
+    link_uri = models.TextField(null=False)
+    link_state = models.TextField(null=False)  # 'ok', 'stale', 'broken'
+    created = models.DateField(null=False)
+    updated = models.DateField(null=False)
+    verified = models.DateField(null=True)
+
+    class Meta:
+        abstract = True    
+    
+class book_links(links):
+    book = models.ForeignKey(
+        'books', 
+        related_name='book_link',
+        on_delete=models.CASCADE,
+    )
+    
+class author_links(links):
+    book = models.ForeignKey(
+        'authors', 
+        related_name='author_link',
+        on_delete=models.CASCADE,
+    )
+    
+"""
+
