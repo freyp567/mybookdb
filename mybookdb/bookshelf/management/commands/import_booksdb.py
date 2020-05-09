9de0e530ebd01a9318e4fb914a4af9c52a5c723c
freyp567 <freyp567@gmail.com>
2020-05-09 14:05:17 +0200

    improved diffing, avoid to overwrite book states

--- mybookdb/bookshelf/management/commands/import_booksdb.py
+++ mybookdb/bookshelf/management/commands/import_booksdb.py
@@ -86,12 +86,36 @@ class Command(BaseCommand):
 
     def __init__(self, stdout=None, stderr=None, no_color=False):
         self._conflicts_books = []
+        self._books_nostate = []
+        self._books_ignorechange =[]
         self._conflicts_authors = []
         super().__init__(stdout, stderr, no_color)
         
     def add_arguments(self, parser):
-        parser.add_argument('dbpath', type=str) # nargs='+', 
+        parser.add_argument('dbpath', type=str) 
+        parser.add_argument('--only', default='')
 
+    def same_value_table(self, table_name, key, curr_value, new_value):
+        """ similar to same_value, but for tables associated to book objects (states, ...) """
+        if curr_value == new_value:
+            return True
+        
+        if isinstance(curr_value, datetime) and not isinstance(new_value, datetime):
+            # avoid rounding errors caused by comparing datetime (tz ware) and date (naive) values
+            new_value = datetime.combine(new_value, datetime.min.time())
+            new_value = new_value.astimezone(timezone.utc)                        
+            
+        if curr_value == new_value:
+            return True
+        
+        if table_name == 'states':
+            if key in ('toRead', 'toBuy', 'haveRead', 'readingNow') and curr_value == True:
+                # keep value if state flag is set, dont reset
+                return None
+            return False
+        
+        return False
+        
         
     def load_table(self, table_name, objType):
         self.stdout.write(f"loading data for table {table_name} ...")
@@ -145,10 +169,12 @@ class Command(BaseCommand):
                         self.stderr.write(f"missing book {book_id} - {data['bookTitle']!r}")
                         continue # do not add if book it relates to does no longer exist
                     
-                elif book_obj.title != data['bookTitle']: # title mismatch?
-                    if book_obj.title in data['bookTitle']:
+                elif book_obj.title != data['bookTitle']: # verify comment for this book
+                    if book_obj.unified_title == data['bookTitle']:
+                        self.stdout.write(f"title mismatch but match on unified title, book {book_obj}")
+                    elif book_obj.title in data['bookTitle']:
                         # e.g. 'Die letzte Legion. Roman' vs 'Die letzte Legion. Roman.'
-                        pass
+                        self.stdout.write(f"title mismatch but match for substring, book {book_obj}")
                     else:
                         #assert book_obj.title == data['bookTitle'], "title mismatch"
                         # pass similarity, e.g. 'Im Land der wei?en Wolke: Roman' vs 'Im Land der weissen Wolke: Roman'
@@ -157,6 +183,16 @@ class Command(BaseCommand):
             elif table_name in ('grBooks','states',) and book_obj is None:
                 # skip if missing book it relates to
                 continue
+            
+            if table_name == 'states':
+                flags_set = [ key for key,value in data.items() if value != 0 and key not in ('id','book',)]
+                if not flags_set:
+                    # make sure that at least 'toRead' is set
+                    self.stdout.write("foce toRead as no status flags set for %s" % book_obj)
+                    data['toRead'] = 1
+               
+            if table_name == 'grBooks':
+                data = data  # TODO normalize to avoid excessive updates
             
             #obj = objType.objects.get(pk=id) # raises DoesNotExist if not found, so avoid
             obj = objType.objects.filter(id=id)
@@ -167,15 +203,16 @@ class Command(BaseCommand):
                 diff = {}
                 for key, value in data.items():
                     obj_value = getattr(obj, key)
-                    if isinstance(obj_value, datetime) and not isinstance(value, datetime):
-                        # avoid rounding errors caused by comparing datetime (tz ware) and date (naive) values
-                        value = datetime.combine(value, datetime.min.time())
-                        value = value.astimezone(timezone.utc)                        
-                    if obj_value != value:
+                    is_same = self.same_value_table(table_name, key, obj_value, value)
+                    if is_same is None:
+                        self.stdout.write(f"ignore change {table_name} {id} key={key} -- book {book_id} {book_obj.title}")
+                        self._books_ignorechange.append(f"change {table_name} key={key} ignored -- book {book_id} {book_obj.title}")
+                    elif is_same is not True:
                         diff[key] = (getattr(obj, key), value) 
+                        
                 if diff:
                     keys = ", ".join(diff.keys())
-                    self.stdout.write(f"update changes to {table_name} {id} keys={keys} - {obj}")
+                    self.stdout.write(f"update changes to {table_name} {id} keys={keys} for book {book_id} {book_obj.title}")
                     # update changed fields
                     for key, value in diff.items():
                         assert key not in ('id',), "not allowed to update %s" % key 
@@ -196,6 +233,13 @@ class Command(BaseCommand):
                 
         self.stdout.write(f"{table_name}: total {updated} rows updated (of total {rowcount})")
         return updated
+
+    def has_author(self, curr_author, new_author):
+        """ mybookdb has both author and translator, want only first """
+        for author in curr_author:
+            if author in new_author:
+                return True
+        return False
         
 
     def update_book_authors(self, book_obj, authors_set, authors_new):
@@ -204,6 +248,9 @@ class Command(BaseCommand):
         if authors_set_ids == authors_new_ids:  # A ^ B, symmetric difference
             return # same, unchanged
 
+        if self.has_author(authors_set, authors_new):
+            return # already have
+            
         id = book_obj.id
         updated = 0
         delta_new = authors_new_ids - authors_set_ids
@@ -236,6 +283,59 @@ class Command(BaseCommand):
         
         # TODO merge new authors created directly in mybookdb with new authors from mybookdb 
         # if same name
+
+    def same_value(self, key, curr_value, new_value):
+        """ compare current and new value for equality """
+        if curr_value == new_value:
+            return True
+        
+        if key in ('reviewsFetchedDate',):
+            # not relevant for update, ignore 
+            return True
+        
+        if not curr_value and not new_value:
+            return True  # ignore pseudo change None vs ''
+        
+        if key == 'title':
+            curr_value = curr_value.strip()
+            new_value =new_value.strip()
+        
+        if key =='orig_description':
+            curr_value = curr_value.replace('\r\n', '\n').strip()
+            new_value = new_value.replace('\r\n', '\n').strip()
+            
+        if key in ('userRating',) and curr_value:
+            # never update if already set
+            return True
+        
+        if key == 'isbn13':
+            if not new_value:
+                # never reset
+                return True
+            if new_value and len(new_value) != 13:
+                # mybookdroid sets isbn13 with isbn10 value by mistake, ignore
+                return True
+        
+        if key =='isbn10':
+            if not new_value:
+                # never reset
+                return True
+        
+        if key in ('subject',):
+            if curr_value and not new_value:
+                # never reset
+                return True
+            
+        if key in ('publisher',):
+            # always ignore updates
+            return True
+        
+        if key == 'state':
+            assert False ## requires more logic when to update
+            return True
+        
+        return curr_value == new_value
+        
 
     def load_books(self):
         self.stdout.write(f"loading data for table books ...")
@@ -285,8 +385,8 @@ class Command(BaseCommand):
                         else:
                             self.stdout.write(f"duplicate author name:{author_name!r}")
                             self._conflicts_authors.append("%s duplicated" % (author_name,))
-                            for author_obj in obj_author:
-                                book_authors.add(author_obj)
+                            #for author_obj in obj_author:
+                            #    book_authors.add(author_obj)
                                 
                             
                 elif col_name in ('reviewsFetchedDate', 'offersFetchedDate', 'created', 'publicationDate'):
@@ -297,8 +397,8 @@ class Command(BaseCommand):
                         data[col_name] = None
                     
                 elif col_name == 'familyName': 
-                    # eliminate redundancy, see ForeignKey set for col authors
-                    pass 
+                    # redundant, ignore
+                    pass
                 
                 elif col_name in ('grBookId', 'googleBookId', 'stateId'):
                     # avoid TypeError: Direct assignment to the reverse side of a related set is prohibited. Use grBookId.set() instead.
@@ -308,29 +408,55 @@ class Command(BaseCommand):
                     data[col_name] = value
                     
             book_obj = books.objects.filter(id=id)
+            if not book_obj:
+                # book not yet synced between mybookdroid and mybookdb, but may be created there with different id
+                # and hopefully identical title; lookup and handle
+                # happens if we import books from wishlist (onleihe Merkzettel), then check it out later on and add to mybookdroid
+                found_by_title = books.objects.exclude(states__obsolete=True).filter(title=book_title)
+                if not found_by_title:
+                    found_by_title = books.objects.exclude(states__obsolete=True).filter(unified_title=book_title)
+                if found_by_title:
+                    assert len(found_by_title) == 1, "title not unique"  # needs more examination, which book to pick 
+                    book_obj2 = found_by_title[0]
+                    sys.stdout.write(f"book does already exist but different id: {book_obj.id}: {book_obj2}")
+                    assert False 
+                    # TODO to be implemented: create new book object with book_obj.id 
+                    # copy all properties, the state, timeline events, bookmarks and comments to new booj_obj
+                    # and delete the excessive book object
+
+
+                assert False, 
+                
             if book_obj:
                 # item does already exist
                 book_obj = book_obj[0]
                 # self.stdout.write(f"existing book {id}: {data['title']!r}") # blather
+                try:
+                    if book_obj.states.obsolete:
+                        self.stdout.write("cannot update book if marked obsolete: %s '%s'" % (id, book_title))
+                        self._conflicts_books.append("%s '%s' -- obsolete" % (id, book_title))
+                        # maybe need to delete in mybookdroid?
+                        continue
+                except states.DoesNotExist:
+                    # RelatedObjectDoesNotExist - rare cases missing States
+                    self.stdout.write("book without state: %s - %s" % (id, book_title))
+                    self._books_nostate.append("%s '%s'" % (id, book_title))
+                
                 diff = {}
-                for key, value in data.items():
-                    if getattr(book_obj, key) != value:
-                        diff[key] = (getattr(book_obj, key), value) 
+                for key, new_value in data.items():
+                    curr_value = getattr(book_obj, key)
+                    if not self.same_value(key, curr_value, new_value):
+                        diff[key] = (curr_value, new_value) 
                         
                 # detect changes in book - authors relationship (many-to-many)
                 book_authors_set = list(book_obj.authors.all())
                 self.update_book_authors(book_obj, book_authors_set, book_authors)
                         
-                diff2 = copy.copy(diff)  # save
-                # discard fields not relevant for update
-                for key in ('reviewsFetchedDate',):
-                    if key in diff:
-                        del diff[key]
                 if diff: # handle other differences
-                    keys = ", ".join(diff2.keys())
+                    keys = ", ".join(diff.keys())
                     self.stdout.write(f"detected changes for book {id} '{book_obj.title}' fields={keys}")
                     # update changed fields, e.g. title
-                    for key, value in diff2.items():
+                    for key, value in diff.items():
                         assert not key in ('id',) # disallow update
                         old_value, new_value = value
                         if key in ('reviewsFechedDate',):
@@ -338,23 +464,29 @@ class Command(BaseCommand):
                         else:
                             value = new_value
                         setattr(book_obj, key, value)
-                    book_obj.updated = datetime.now(tz=timezone.utc)  #.isoformat()[:10]
+                    book_obj.updated = datetime.now(tz=timezone.utc)
                     book_obj.save()
                 else:
                     pass # no changes
                     
             else:
-                found_by_title = books.objects.filter(title=book_title)
+                found_by_title = books.objects.exclude(states__obsolete=True).filter(title=book_title)
                 if found_by_title:
                     if not books.objects.filter(id=id):
                         other_book = found_by_title[0]
                         # extract book_links, if any
                         # links = book_links.objects.find(id=other_book.id)
                         # save title if edited 
+                        assert False # TODO ro be resolved, cause of duplicated books
+                        # e.g. from Merkzettel imported to mybookdb (756 Der Circle)
+                        # later on checked out in onleihe and added to mybookdroid - as new book
+                        # book with lower id has precedence, needs to be update with info from other book
+                        # for description, urls, ...
+                        
                         # diffing ?
                         self.stdout.write(f"already in mybooksdb with different id than {id}: {data['title']!r}")
                         data['new_description'] = other_book.description  # retain (edited) description
-                        self._conflicts_books.add("%s '%s'" % (id, book_title))
+                        self._conflicts_books.append("%s '%s'" % (id, book_title))
                     else:
                         assert False, "duplicate in mybooksdb: {data['title']!r}"  # or id collision
                 self.stdout.write(f"new book {id}: {data['title']!r}")
@@ -388,20 +520,44 @@ class Command(BaseCommand):
         conn.set_trace_callback(self.output_sqllite)
         self._c = conn.cursor()
         
-        self.load_table('authors', authors)
-        self.load_table('groups', groups)
+        only = [tn.strip() for tn in options.get('only', '').split(',')]
+        only = [tn for tn in only if tn]  # drop empty entries
+        if not only or 'authors' in only:
+            self.load_table('authors', authors)
+
+        if not only or 'groups' in only:
+            self.load_table('groups', groups)
             
-        self.load_books()
-
-        self.load_table('states', states)
-        # self.load_table('bookGroup', bookGroup) # TODO 
-
-        self.load_table('googleBooks', googleBooks)
-        self.load_table('grBooks', grBooks)        
-        self.load_table('comments', comments)
+        if not only or 'books' in only:
+            self.load_books()
+    
+        if not only or 'states' in only:
+            self.load_table('states', states)
+            
+        if not only or 'states' in only:
+            skipped = 1  # self.load_table('bookGroup', bookGroup) # not synced
+    
+        if not only or 'googleBooks' in only:
+            self.load_table('googleBooks', googleBooks)
+            
+        if only and 'grBooks' in only:
+            TODO = 1  # self.load_table('grBooks', grBooks)  # TODO diffing needs to be fixed
+            
+        if only and 'comments' in only:
+            self.load_table('comments', comments)
         
         self.stdout.write(self.style.SUCCESS(f"import books.db succeeded"))
 
+        if self._books_ignorechange:
+            self.stderr.write("ignored changes:\n")
+            self.stderr.write("%s\n" % "\n".join(self._books_ignorechange))
+            self.stderr.write(".\n")            
+            
+        if self._books_nostate:
+            self.stderr.write("books without state:\n")
+            self.stderr.write("%s\n" % "\n".join(self._books_nostate))
+            self.stderr.write(".\n")            
+            
         if self._conflicts_books:
             self.stderr.write("conflicts for books:\n")
             self.stderr.write("%s\n" % "\n".join(self._conflicts_books))
